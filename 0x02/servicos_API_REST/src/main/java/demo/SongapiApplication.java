8de30bebe4d6921eac9b981de719bcb3c00bc95b
David <davidwatsonbr@gmail.com>
2025-09-18 13:40:08 -0300

    subindo exercicios na estrutura esperada

---
+++ 0x02/servicos_API_REST/src/main/java/demo/SongapiApplication.java
@@ -0,0 +1,13 @@
+package src.main.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongapiApplication.class, args);
+	}
+
+}
