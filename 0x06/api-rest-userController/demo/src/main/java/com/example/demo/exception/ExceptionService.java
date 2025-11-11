17eb5d1a844d843df77bf9aff2e58064c4b69e46
David <davidwatsonbr@gmail.com>
2025-11-11 09:33:00 +0000

    ajuste classe

--- 0x06/api-rest-userController/demo/src/main/java/com/example/demo/exception/ExceptionService.java
+++ 0x06/api-rest-userController/demo/src/main/java/com/example/demo/exception/ExceptionService.java
@@ -17,4 +17,20 @@ public class ExceptionService {
         uer.setMessage("You have entered CPF " + err.getMessage() + " invalid.");
         return new ResponseEntity(uer, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    ResponseEntity<UserIdException> handleException(UserIdException err) {
+        UserErrorResponse uer = new UserErrorResponse();
+        uer.setStatus(HttpStatus.BAD_REQUEST.value());
+        uer.setMessage("You have entered ID " + err.getMessage() + " invalid.");
+        return new ResponseEntity(uer, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    ResponseEntity<UserNameException> handleException(UserNameException err) {
+        UserErrorResponse uer = new UserErrorResponse();
+        uer.setStatus(HttpStatus.BAD_REQUEST.value());
+        uer.setMessage("You have entered USERNAME " + err.getMessage() + " invalid.");
+        return new ResponseEntity(uer, HttpStatus.BAD_REQUEST);
+    }
 }
